8314b294013fb22a530dbdd93cb476f282159ee0
Saevar2000 <saevar2000@gmail.com>
2018-04-02 23:00:41 +0000

    change 'done' to 'finished'

--- src/mock-data/mock-missions.ts
+++ src/mock-data/mock-missions.ts
@@ -2,8 +2,8 @@ import { Mission } from "../models/mission";
 
 export const MISSIONS: Mission[] = [
     { Id: 1, Threat: "C-137 Buttworld", Detail: "The butts have taken over, please help us", Status: 'Pending' },
-    { Id: 2, Threat: "Cronenberg World", Detail: "The Cronenberg's are infecting everyone!!", Status: 'Done' },
-    { Id: 3, Threat: "C-137 Earth", Detail: "Cat Rick has attacked us and is turning everyone into cats", Status: 'Done' },
+    { Id: 2, Threat: "Cronenberg World", Detail: "The Cronenberg's are infecting everyone!!", Status: 'Finished' },
+    { Id: 3, Threat: "C-137 Earth", Detail: "Cat Rick has attacked us and is turning everyone into cats", Status: 'Finished' },
     { Id: 4, Threat: "X-304 Gazorpazorp", Detail: "We are stuck with these primitive males", Status: 'Pending' },
     { Id: 5, Threat: "J197 Saturn", Detail: "There are creatures holding us with their farting powers", Status: 'Denied' },
     { Id: 4, Threat: "C-135 Ijinnil", Detail: "We have been imprisoned, help!", Status: 'Pending' },
